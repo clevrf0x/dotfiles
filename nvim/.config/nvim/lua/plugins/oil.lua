42f652ccccd9c0cb2d5bb44b7cc73f0dc084f9d7
clevrf0x <favasm72@gmail.com>
2024-07-28 23:19:15 +0530

    Removed lazy loading for oil.nvim

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,10 +1,11 @@
 return {
-  'stevearc/oil.nvim',
+  "stevearc/oil.nvim",
+  lazy = false,
   opts = {},
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("oil").setup()
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  end
+  end,
 }
